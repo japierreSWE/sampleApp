1b3f45f4b10f4a0acb474f644bffc9f4e65d392a
japierre <jean-philippe@autoreg-118062.dyn.wpi.edu>
2019-05-20 11:49:06 -0400

    Creating an account adds user info to db.

--- app/src/main/java/com/example/sampleapp/CreateAccountActivity.java
+++ app/src/main/java/com/example/sampleapp/CreateAccountActivity.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -22,12 +26,14 @@ import java.util.Locale;
 public class CreateAccountActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
     }
 
     /** Makes a toast show up.
@@ -87,12 +93,31 @@ public class CreateAccountActivity extends AppCompatActivity {
 
                         //if the account was created, go back to start page.
                         if(task.isSuccessful()) {
-                            Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
-                            startActivity(intent);
+                            Log.d("CreateAccountActivity", "Made user");
                         } else {
                             makeToast("Unable to create account.");
                         }
 
+                    }
+                });
+
+        User user = new User(email, dob, name, password);
+
+        //add the user's info to the database.
+        db.collection("users").document(email).set(user)
+                //go back to start page if successful.
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        makeToast("Internal error. Please try again.");
+                        Log.w("CreateAccountActivity", "Couldn't write document", e);
                     }
                 });
 
---
+++ app/src/main/java/com/example/sampleapp/User.java
@@ -0,0 +1,19 @@
+package com.example.sampleapp;
+
+public class User {
+
+    String email; //the user's email
+    String dob; //the user's date of birth
+    String name; //the user's name
+    String password; //the user's password
+
+    public User(String email, String dob, String name, String password) {
+
+        this.email = email;
+        this.dob = dob;
+        this.name = name;
+        this.password = password;
+
+    }
+
+}
