833c72a5343d4813b1ba7f21d0577eb7b77680f5
japierre <jean-philippe@autoreg-118062.dyn.wpi.edu>
2019-05-23 14:42:47 -0400

    Wrote code that shows the route, but there is an error to be fixed.

--- app/src/main/java/com/example/sampleapp/ViewDoctorActivity.java
+++ app/src/main/java/com/example/sampleapp/ViewDoctorActivity.java
@@ -8,14 +8,19 @@ import android.Manifest;
 import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
@@ -28,7 +33,16 @@ import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.DirectionsStep;
+import com.google.maps.model.EncodedPolyline;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -197,6 +211,89 @@ public class ViewDoctorActivity extends AppCompatActivity implements OnMapReadyC
 
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
+
+        List<LatLng> path = new ArrayList<LatLng>();
+
+        //start by setting up the directions API request
+        GeoApiContext context = new GeoApiContext.Builder().apiKey("AIzaSyDfU9D8p_AoXAQzATv_u90fX97LPtls55k").build();
+        String userCoords = String.valueOf(userLocation.getLatLng().latitude) + "," +  String.valueOf(userLocation.getLatLng().longitude);
+        String doctorCoords = String.valueOf(doctorLocation.getLatLng().latitude) + "," +  String.valueOf(doctorLocation.getLatLng().longitude);
+        DirectionsApiRequest req = DirectionsApi.getDirections(context, userCoords, doctorCoords);
+
+        //loop through legs and steps to get encoded polylines of each step
+        try {
+
+            DirectionsResult result = req.await();
+
+            //if there are routes, use the first one.
+            if(result.routes != null && result.routes.length > 0) {
+                DirectionsRoute route = result.routes[0];
+
+                //if there are legs, do the following:
+                if(route.legs != null) {
+                    for(int i = 0; i<route.legs.length; i++) {
+                        DirectionsLeg leg = route.legs[i];
+
+                        //if there are steps, do the following:
+                        if(leg.steps != null) {
+
+                            for(int j = 0; j<leg.steps.length; j++) {
+
+                                DirectionsStep step = leg.steps[j];
+                                //get the details within each step.
+                                if(step.steps != null && step.steps.length > 0) {
+
+                                    for(int k = 0; k<step.steps.length; k++) {
+                                        DirectionsStep step1 = step.steps[k];
+                                        EncodedPolyline points1 = step1.polyline;
+
+                                        if(points1 != null) {
+                                            //decode polyline and add points to list
+                                            List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
+                                            for(com.google.maps.model.LatLng coord1 : coords1) {
+                                                path.add(new LatLng(coord1.lat,coord1.lng));
+                                            }
+
+                                        }
+
+                                    }
+
+                                } else {
+                                    //if we can't get the step details, proceed
+                                    //with  adding the points to the list
+                                    EncodedPolyline points = step.polyline;
+                                    if(points != null) {
+                                        //decode polyline and add points to list
+                                        List<com.google.maps.model.LatLng> coords = points.decodePath();
+                                        for (com.google.maps.model.LatLng coord : coords) {
+                                            path.add(new LatLng(coord.lat, coord.lng));
+                                        }
+                                    }
+                                }
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        } catch(Exception e) {
+            Log.e("ViewDoctorActivity", e.toString(), e);
+        }
+
+        //the following code displays the line.
+        if(path.size() > 0) {
+            PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLUE).width(5);
+            map.addPolyline(opts);
+        }
+
+        map.getUiSettings().setZoomControlsEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation.getLatLng(), 6));
+
     }
 
 
